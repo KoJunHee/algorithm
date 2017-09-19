4a5a0ad4e94ab28fd972adcce8f03c1f09fe9589
jay <kjh338076@naver.com>
2017-09-19 23:26:14 +0900

    2 x n 타일리 문제 추가

--- src/bj_11726/Main.java
+++ src/bj_11726/Main.java
@@ -1,4 +1,24 @@
 package bj_11726;
 
-public class Main {
+import java.util.Scanner;
+
+public class Main{
+
+    public static int d[];
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        d = new int[n+1];
+        System.out.println(solve(n)%10007);
+    }
+
+    static int solve(int n){
+        if(n==1)
+            return 1;
+        else if(n==2)
+            return 2;
+        else
+            return solve(n-1) + solve(n-2);
+
+    }
 }
