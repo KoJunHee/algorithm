61b456c1bda1714f0230a7bc29da51a91fce7686
jay <kjh338076@naver.com>
2017-11-30 00:33:39 +0900

    트리의 부모 찾기

--- src/bj_11725/Main.java
+++ src/bj_11725/Main.java
@@ -1,20 +1,11 @@
 package bj_11725;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-
-    //탐색
-    public static int preorder(int a[][], int x, int find, int parent) {
-
-        if (x == find) {
-            return parent;
-        }
-
-        preorder(a, a[x][0], find, x);
-        preorder(a, a[x][1], find, x);
-        return 0;
-    }
 
 
     public static void main(String[] args) {
@@ -22,52 +13,69 @@ public class Main {
 
         //노드의 개수
         int n = scanner.nextInt();
-        scanner.nextLine();
 
-        //왼쪽자식, 오른쪽자식, 부모 저장할 배열
-        int a[][] = new int[n + 1][3];
+        //인접리스트 생성
+        ArrayList<Integer>[] a = (ArrayList<Integer>[]) new ArrayList[n + 1];
+        for(int i=1; i<=n ; i++){
+            a[i] = new ArrayList<Integer>();
+        }
 
+        //트리 그리기
+        for (int i = 1; i <= n-1; i++) {
 
-        for (int i = 1; i <= n - 1; i++) {
+            //입력
+            int u = scanner.nextInt();
+            int v = scanner.nextInt();
 
-            //input
-            String input = scanner.nextLine();
-            int l = input.charAt(0);
-            int r = input.charAt(2);
-
-            //탐색
-            int parent = preorder(a, 1, l, 0);
-
-
-            //찾음
-            if (parent != 0) {
-
-                //왼쪽 자식 비었으면
-                if (a[l][0] != 0)
-                    a[l][0] = r;
-                //왼쪽 자식이 찼으면
-                else
-                    a[l][1] = r;
-
-                a[l][2] = parent;
-
-            }
-            //못 찾음
-            else {
-
-                //두번째 수의 왼쪽비였으면
-                if(a[r][0]!=0)
-                    a[r][0]=l;
-                else
-                    a[r][1]=l;
-
-
-
-
-            }
-
+            //저장
+            a[u].add(v);
+            a[v].add(u);
 
         }
+
+        //방문 체크 배열
+        boolean check[] = new boolean[n + 1];
+
+        //부모 저장 배열
+        int parents[] = new int[n + 1];
+
+        //큐 생성
+        Queue<Integer> queue = new LinkedList<Integer>();
+
+        //초기값 push, 방문체크
+        queue.add(1);
+        check[1] = true;
+
+        //bfs
+        while (!queue.isEmpty()) {
+
+            //queue pop
+            int popData = queue.remove();
+
+            //자식들 탐색
+            for (int child : a[popData]) {
+
+                //방문하지 않았으면
+                if (!check[child]) {
+
+                    //방문했다고 체크
+                    check[child] = true;
+
+                    //queue에 push
+                    queue.add(child);
+
+                    //부모 저장
+                    parents[child] = popData;
+
+                }
+            }
+        }
+
+        //출력
+        for (int i = 2; i <= n; i++) {
+            System.out.println(parents[i]);
+        }
+
 
     }
 }
