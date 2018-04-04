aec202f484bfa7f6c827b576c118867f214b069e
jay <kjh338076@naver.com>
2018-04-04 13:31:46 +0900

    영역구하기

---
+++ src/bj_2583/Main.java
@@ -0,0 +1,108 @@
+package bj_2583;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+
+    static boolean a[][];
+    static boolean visit[][];
+    static int m, n, k;
+    static ArrayList<Integer> areas;
+    static int dx[] = {0, -1, 0, 1};
+    static int dy[] = {1, 0, -1, 0};
+
+
+    public static void main(String[] args) {
+
+        // 초기화
+        init();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                //방문하지 않았고, 이미 칠해져 있는 곳이 아니면
+                if (!visit[i][j] && !a[i][j]) {
+
+                    //방문 체크
+                    visit[i][j] = true;
+                    areas.add(search(i, j));
+                }
+            }
+        }
+
+        //정렬
+        Collections.sort(areas);
+
+        //출력
+        System.out.println(areas.size());
+        for (int i = 0; i < areas.size(); i++) {
+            System.out.print(areas.get(i) + " ");
+        }
+
+    }
+
+    // 탐색
+    public static int search(int y, int x) {
+
+
+        int sum = 1;
+
+        //동서남북
+        for (int q = 0; q < 4; q++) {
+            if(check(y + dy[q], x + dx[q])){
+                visit[y + dy[q]][x + dx[q]] = true;
+                sum += search(y + dy[q], x + dx[q]);
+            }
+        }
+
+        return sum;
+    }
+
+    // 체크
+    public static boolean check(int y, int x) {
+
+        // 종이 밖
+        if (y < 0 || y >= m || x < 0 || x >= n) {
+            return false;
+        }
+
+        // 이미 칠해져 있거나 방문햇으면
+        if (visit[y][x] || a[y][x]) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void init() {
+        Scanner scanner = new Scanner(System.in);
+
+        m = scanner.nextInt();
+        n = scanner.nextInt();
+        k = scanner.nextInt();
+        areas = new ArrayList<Integer>();
+
+        a = new boolean[m][n];
+        visit = new boolean[m][n];
+
+
+        for (int i = 0; i < k; i++) {
+            int x1 = scanner.nextInt();
+            int y1 = scanner.nextInt();
+            int x2 = scanner.nextInt();
+            int y2 = scanner.nextInt();
+
+            //직사각형 부분 색칠
+            for (int x = x1; x < x2; x++) {
+                for (int y = y1; y < y2; y++) {
+                    a[y][x] = true;
+                }
+            }
+
+        }
+    }
+}
