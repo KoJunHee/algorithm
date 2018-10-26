494113bd9cf3b9411dd3f6267cf11c2393685010
jay <kjh338076@naver.com>
2018-10-26 18:31:59 +0900

    가르침 ing

---
+++ src/bj_1062/Main.java
@@ -0,0 +1,95 @@
+package bj_1062;
+
+import java.util.Scanner;
+
+public class Main {
+    static int n, k;
+    static int ans = Integer.MIN_VALUE;
+    static String[] strings;
+
+    public static void main(String[] args) {
+        input();
+        solve();
+    }
+
+    public static void input() {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        k = scanner.nextInt();
+        scanner.nextLine();
+        strings = new String[n];
+        for (int i = 0; i < n; i++) {
+            strings[i] = scanner.nextLine();
+        }
+    }
+
+    public static void solve() {
+        for (int i = 0; i < n; i++) {
+            dfs(i, new boolean[n], new boolean[26], k, 1, 1);
+        }
+        System.out.println(ans);
+    }
+
+    public static void dfs(int curIdx, boolean idxArr[], boolean alphabetArr[], int teachableCnt, int depthCnt, int wordCnt) {
+        //copy
+        boolean copyIdxArr[] = new boolean[n];
+        boolean copyAlphabetsArr[] = new boolean[26];
+        for (int i = 0; i < n; i++) {
+            copyIdxArr[i] = idxArr[i];
+        }
+        for (int i = 0; i < 26; i++) {
+            copyAlphabetsArr[i] = alphabetArr[i];
+        }
+
+        //방문 체크
+        copyIdxArr[curIdx] = true;
+
+        //새로 배울 문자 개수 체크
+        String curString = strings[curIdx];
+        boolean temp[] = new boolean[26];
+        for (int i = 0; i < curString.length(); i++) {
+            char ch = curString.charAt(i);
+            int idx = (int) ch - 97;
+            if (!copyAlphabetsArr[idx]) {
+                temp[idx] = true;
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < 26; i++) {
+            if (temp[i]) {
+                cnt++;
+            }
+        }
+
+        //새로 배워야하는 단어 개수가, 배울 수 있는 단어 개수 보다 작거나 같으면
+        //배웠다고 체크
+        if (cnt <= teachableCnt) {
+            wordCnt++;
+            for (int i = 0; i < curString.length(); i++) {
+                char ch = curString.charAt(i);
+                int idx = (int) ch - 97;
+                copyAlphabetsArr[idx] = true;
+            }
+            teachableCnt -= cnt;
+        }
+
+        if (depthCnt == n) {
+            System.out.println(wordCnt);
+
+            ans = wordCnt > ans ? wordCnt : ans;
+            return;
+        }
+
+
+        //next
+        for (int i = 0; i < n; i++) {
+            if (!copyIdxArr[i]) {
+                dfs(i, copyIdxArr, copyAlphabetsArr, teachableCnt, depthCnt++, wordCnt);
+            }
+        }
+
+        copyIdxArr[curIdx] = false;
+
+
+    }
+}
