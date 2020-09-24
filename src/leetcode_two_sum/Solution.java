e72837452579e5af011629390d391b16ed4a87b6
jko <junheee.ko@gmail.com>
2020-09-25 00:49:38 +0900

    ADD : leetcode - two sum

---
+++ src/leetcode_two_sum/Solution.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/two-sum/
+package leetcode_two_sum;
+
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            int firstNum = nums[i];
+
+            for (int j = i + 1; j < nums.length; j++) {
+                int secondNum = nums[j];
+                if (firstNum + secondNum == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
+
+
+
