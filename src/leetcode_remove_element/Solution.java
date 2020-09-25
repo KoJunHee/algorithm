e1c4015fdf972fcdef4117e4b35a69234d5d13de
jko <junheee.ko@gmail.com>
2020-09-25 23:50:23 +0900

    ADD : leetcode_remove_element

--- src/leetcode_remove_element/Solution.java
+++ src/leetcode_remove_element/Solution.java
@@ -1,14 +1,14 @@
+// https://leetcode.com/problems/remove-element/submissions/
+
 package leetcode_remove_element;
 
 public class Solution {
     public int removeElement(int[] nums, int val) {
         int i = 0;
         for (int j = 0; j < nums.length; j++) {
-            if (nums[j] == val) {
-                continue;
+            if (nums[j] != val) {
+                nums[i++] = nums[j];
             }
-
-            nums[i++] = nums[j];
         }
 
         return i;
