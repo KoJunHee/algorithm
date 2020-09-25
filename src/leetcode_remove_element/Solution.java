822af97adeab774e828a0a07b39c3cc1c08f3fd8
jko <junheee.ko@gmail.com>
2020-09-25 23:47:23 +0900

    ADD : gitignore

---
+++ src/leetcode_remove_element/Solution.java
@@ -0,0 +1,16 @@
+package leetcode_remove_element;
+
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] == val) {
+                continue;
+            }
+
+            nums[i++] = nums[j];
+        }
+
+        return i;
+    }
+}
