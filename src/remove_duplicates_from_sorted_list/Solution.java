98af473a48feac22ddd64fa3ac20aa9aff07a3ac
jko <junheee.ko@gmail.com>
2020-10-02 19:26:53 +0900

    ADD : remove_duplicates_from_sorted_list

---
+++ src/remove_duplicates_from_sorted_list/Solution.java
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/solution
+package remove_duplicates_from_sorted_list;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode i = head;
+        ListNode j = i.next;
+
+        while (j != null) {
+            if (i.val != j.val) {
+                i.next = j;
+                i = j;
+                j = i.next;
+            } else {
+                j = j.next;
+            }
+        }
+
+        i.next = j;
+
+        return head;
+    }
+}
